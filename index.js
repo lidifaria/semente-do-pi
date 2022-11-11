95cc7e7102380774a7a60a9a4b0e58b0c6010278
Lidi Faria <falecom@lidifaria.com>
2022-11-11 13:33:32 -0300

    Inclusão de página produtos, index, public, css, img e views

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Está config");
+// Importar o express
+// Definir uma constante e "guardar" express dentro dela
+
+const express = require('express');
+
+// Criar o servidor
+// normalmente chamado de app, mas hj chamaremos de servidor
+// express é uma função
+
+const servidor = express();
+
+// Definir a public como pasta de arquivos estáticos
+
+servidor.use(express.static('public'));
+
+// Definir as rotas
+servidor.get(
+    '/produtos',             // endereço ou caminho a receber a requisição
+    (req,res) => {           // função que será executada quando a requisição chegar
+        return res.sendFile(__dirname + '/views/produtos.html');
+    }                     
+)
+
+// Fazer meu servidor rodar
+servidor.listen(3000);
